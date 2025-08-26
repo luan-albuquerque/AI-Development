5484ed6c536386317033aa4e9b4a53bf674d2c06
LUAN ALBUQUERQUE DOS SANTOS <luan.santos@indt.org.br>
2025-08-25 23:12:22 -0400

    feat: Finalizando Modelo de Regressão Linear

---
+++ IA-Linear-Regression/api_modelo.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+import uvicorn 
+import joblib
+
+
+
+# Criar uma instância do FastAPI
+
+app = FastAPI()
+
+
+class request_body(BaseModel):
+    horas_estudo: float
+
+
+
+#Carregar modelo para realizar a predição
+
+
+modelo_pontuacao = joblib.load('./modelo_regressao.pkl')
+
+@app.post('/predict')
+def predict(data: request_body):
+    # Preparar os dados para predição
+    input_feature = [[data.horas_estudo]]
+
+    # Realizar a predição
+    y_pred = modelo_pontuacao.predict(input_feature)[0].astype(int)
+
+    return { 'pontuacao_teste' : y_pred.tolist() }
+
